dce11f041c130688dc43ff8ce6a88f3d615c8e6f
AloofSkip <nic.veloce@gmail.com>
2022-10-05 21:36:47 -0700

    create next.js project with jest, react testing library, eslint and prettier for frontend of project.

---
+++ src/ui/__tests__/index.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import Home from '../pages/index';
+import '@testing-library/jest-dom';
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />);
+
+    const heading = screen.getByRole('heading', {
+      name: /Job Tracker/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
