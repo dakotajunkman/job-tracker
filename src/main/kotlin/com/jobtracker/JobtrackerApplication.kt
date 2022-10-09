0b841d36089870ac896dbdf575c94217a6af0c2e
Dakota Junkman <dakota.junkman@gmail.com>
2022-10-09 00:58:52 -0500

    create db connection

--- src/main/kotlin/com/jobtracker/JobtrackerApplication.kt
+++ src/main/kotlin/com/jobtracker/JobtrackerApplication.kt
@@ -1,11 +1,26 @@
 package com.jobtracker
 
+import db.DatabaseConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import javax.sql.DataSource
 
+/**
+ * Anything in this class will be called at start up
+ * @Bean annotation creates a singleton
+ */
 @SpringBootApplication
-class JobtrackerApplication
+class JobtrackerApplication {
+    @Bean(name = ["db"])
+    fun createDb(): DataSource {
+        return DatabaseConfig().createConnectionPool()
+    }
 
-fun main(args: Array<String>) {
-    runApplication<JobtrackerApplication>(*args)
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<JobtrackerApplication>(*args)
+        }
+    }
 }
---
+++ src/main/kotlin/db/DatabaseConfig.kt
@@ -0,0 +1,29 @@
+package db
+
+import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Scope
+import javax.swing.SwingContainer
+
+@Configuration
+@Scope("singleton")
+class DatabaseConfig {
+    val CON_NAME = System.getenv("DB_CON")
+    val DB_USER = System.getenv("DB_USER")
+    val DB_PW = System.getenv("DB_PW")
+    val DB_NAME =System.getenv("DB_NAME")
+
+    fun createConnectionPool(): DataSource {
+        val config = HikariConfig()
+        config.jdbcUrl = String.format("jdbc:postgresql:///%s", DB_NAME)
+        config.username = DB_USER
+        config.password = DB_PW
+
+        config.addDataSourceProperty("socketFactory", "com.google.cloud.sql.postgres.SocketFactory")
+        config.addDataSourceProperty("cloudSqlInstance", CON_NAME)
+        return HikariDataSource(config)
+    }
+}
