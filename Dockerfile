32326c21a85d134ea1e529e7de1b756ae8d37adf
Dakota Junkman <dakota.junkman@gmail.com>
2022-10-09 18:11:04 -0500

    creds

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,5 @@
 FROM tomcat:9.0.65-jdk17
 RUN rm -rf /usr/local/tomcat/webapps/*
 COPY ./build/libs/jobtracker-0.0.1-SNAPSHOT.war /usr/local/tomcat/webapps
+COPY ./config.json .
+ENV GOOGLE_APPLICATION_CREDENTIALS=./config.json
--- setenv.sh
+++ setenv.sh
@@ -4,6 +4,8 @@ export DB_CON=$DB_CON
 export DB_NAME=$DB_NAME
 export DB_USER=$DB_USER
 export DB_PW=$DB_PW
+echo $CREDS > config.json
+cat config.json
 echo ENV DB_CON="$DB_CON" >> Dockerfile
 echo ENV DB_NAME="$DB_NAME" >> Dockerfile
 echo ENV DB_USER="$DB_USER" >> Dockerfile
