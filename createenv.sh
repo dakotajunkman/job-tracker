51e5540cbc77a10f2cb6055f8df2689eb7bccb13
Dakota Junkman <dakota.junkman@gmail.com>
2022-10-09 22:43:58 -0500

    update shell script

--- createenv.sh
+++ createenv.sh
@@ -4,10 +4,4 @@ export DB_CON=$DB_CON
 export DB_NAME=$DB_NAME
 export DB_USER=$DB_USER
 export DB_PW=$DB_PW
-echo $CREDS > config.json
-cat config.json
-echo ENV DB_CON="$DB_CON" >> Dockerfile
-echo ENV DB_NAME="$DB_NAME" >> Dockerfile
-echo ENV DB_USER="$DB_USER" >> Dockerfile
-echo ENV DB_PW="$DB_PW" >> Dockerfile
-cat Dockerfile
+
