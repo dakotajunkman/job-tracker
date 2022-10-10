29a3da821106d48cda6c229be63c86c585588e2d
Dakota Junkman <dakota.junkman@gmail.com>
2022-10-10 00:12:48 -0500

    add all to createenv

--- createenv.sh
+++ createenv.sh
@@ -5,3 +5,9 @@ export DB_NAME=$DB_NAME
 export DB_USER=$DB_USER
 export DB_PW=$DB_PW
 
+# Copy env variables to application.properties
+echo spring.cloud.gcp.sql.database-name="$DB_NAME" >> ./src/main/resources/application.properties
+echo spring.cloud.gcp.sql.instance-connection-name="$DB_CON" >> ./src/main/resources/application.properties
+echo spring.datasource.username="$DB_USER" >> ./src/main/resources/application.properties
+echo spring.datasource.password="$DB_PW" >> ./src/main/resources/application.properties
+cat ./src/main/resources/application.properties
