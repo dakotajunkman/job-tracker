0d79c564ffb0f9b95e933b522c3e2c9efadea20a
HudsonSoutheyGordon <southeyh@oregonstate.edu>
2022-10-11 21:36:27 -0400

    Add Entity V1

--- src/main/kotlin/com/jobtracker/api/repository/entities/UserEntity.kt
+++ src/main/kotlin/com/jobtracker/api/repository/entities/UserEntity.kt
@@ -13,6 +13,9 @@ data class UserEntity(
     @Column(name = "first_name", nullable = false) val firstName: String,
     @Column(name = "last_name", nullable = false) val lastName: String,
     @Column(name = "email_address", nullable = false) val emailAddress: String
+
+    @OneToMany(mappedBy = "user")
+    var applications: List<ApplicationEntity>
 ) {
   override fun toString() =
       "User type with id: $id, firstName: $firstName, lastName: $lastName, email: $emailAddress"
