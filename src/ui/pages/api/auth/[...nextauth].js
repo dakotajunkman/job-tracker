76b0586ef20e9174f71bdbac3844fd3f9c0a4884
Nic Nolan <nic.veloce@gmail.com>
2022-10-18 09:53:07 -0700

    add secret key to auth options (#12)

--- src/ui/pages/api/auth/[...nextauth].js
+++ src/ui/pages/api/auth/[...nextauth].js
@@ -8,6 +8,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async jwt({token, account}) {
       // Persist the OAuth access_token to the token right after signin
