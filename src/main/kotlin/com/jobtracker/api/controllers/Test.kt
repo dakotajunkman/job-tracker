982e370fc6b10336a0854b9e2a8b3cb1f7d01aa0
Dakota Junkman <dakota.junkman@gmail.com>
2022-10-17 21:04:57 -0500

    add ping endpoint

--- src/main/kotlin/com/jobtracker/api/controllers/Test.kt
+++ src/main/kotlin/com/jobtracker/api/controllers/Test.kt
@@ -1,14 +1,20 @@
 package com.jobtracker.api.controllers
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-data class Test(val message: String)
+data class Ping(val message: String)
 
 @RestController
+@RequestMapping("/api")
 class TestController {
-    @RequestMapping("/")
-    fun message(): Test {
-        return Test("Hello")
+    @GetMapping("/ping")
+    fun message(): ResponseEntity<Ping> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(Ping("Hi Bubba"))
     }
 }
