ee5614e0344abd60bfbe6344b0e9e54784ea1d32
Dakota Junkman <dakota.junkman@gmail.com>
2022-10-19 18:03:34 -0500

    remove nullable id

--- src/main/kotlin/com/jobtracker/api/controllers/models/CompanyModel.kt
+++ src/main/kotlin/com/jobtracker/api/controllers/models/CompanyModel.kt
@@ -3,11 +3,8 @@ package com.jobtracker.api.controllers.models
 import com.jobtracker.api.repository.entities.CompanyEntity
 import java.util.UUID
 
-data class CompanyModel(val name: String, var companyId: UUID? = null) {
-  init {
-    if (companyId == null) companyId = UUID.randomUUID()
-  }
-  fun toCompanyEntity() = CompanyEntity(companyId!!, name)
+data class CompanyModel(val name: String, var companyId: UUID = UUID.randomUUID()) {
+  fun toCompanyEntity() = CompanyEntity(companyId, name)
 }
 
 data class MultipleCompanyModel(val companies: MutableList<CompanyModel>)
