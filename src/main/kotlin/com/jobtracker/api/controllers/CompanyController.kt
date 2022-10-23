2340c7525e24de8533be70269066276e2b307371
HudsonSoutheyGordon <southeyh@oregonstate.edu>
2022-10-23 19:02:28 -0400

    First Attempt at GET endpoint - contacts

--- src/main/kotlin/com/jobtracker/api/controllers/CompanyController.kt
+++ src/main/kotlin/com/jobtracker/api/controllers/CompanyController.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
--- src/main/kotlin/com/jobtracker/api/controllers/ContactController.kt
+++ src/main/kotlin/com/jobtracker/api/controllers/ContactController.kt
@@ -5,14 +5,12 @@ import com.jobtracker.api.controllers.models.ContactModel
 import com.jobtracker.api.controllers.models.ErrorModel
 import com.jobtracker.api.repository.ContactRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.oauth2.jwt.JwtDecoder
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/api")
@@ -24,7 +22,7 @@ class ContactController(
     @Autowired
     val converter: Converter
 ) {
-    @PostMapping("/contacts")
+    @PostMapping("/contacts}")
     fun createContact(
         @RequestBody contact: ContactModel,
         @RequestHeader("Authorization") token: String):ResponseEntity<Any> {
@@ -37,4 +35,16 @@ class ContactController(
         val saved = contactRepository.save(contact.toContactEntity(companyObj, applicationObjs))
         return ResponseEntity.status(HttpStatus.CREATED).body(saved)
     }
+
+    @GetMapping("/contacts/{contactID}")
+    fun getContact(
+        @PathVariable contactID: UUID,
+        @RequestHeader("Authorization") token: String):ResponseEntity<Any> {
+
+        val retrieved = contactRepository.findByIdOrNull(contactID)
+            ?: return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorModel(404, "Contact with ID does not exist"))
+
+        return ResponseEntity.status(HttpStatus.OK).body(retrieved)
+    }
+
 }
