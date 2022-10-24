e09536e58895fbf110a6ebc872e0cf4d84e4be7f
HudsonSoutheyGordon <southeyh@oregonstate.edu>
2022-10-24 17:52:13 -0400

    WIP FindByEmail

--- src/main/kotlin/com/jobtracker/api/controllers/ApplicationController.kt
+++ src/main/kotlin/com/jobtracker/api/controllers/ApplicationController.kt
@@ -4,6 +4,7 @@ import com.jobtracker.api.business.Converter
 import com.jobtracker.api.controllers.models.ApplicationModel
 import com.jobtracker.api.controllers.models.ErrorModel
 import com.jobtracker.api.repository.ApplicationRepository
+import com.jobtracker.api.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -20,6 +21,8 @@ class ApplicationController(
     @Autowired
     val applicationRepository: ApplicationRepository,
     @Autowired
+    val userRepository: UserRepository,
+    @Autowired
     val jwtDecoder: JwtDecoder,
     @Autowired
     val converter: Converter
@@ -33,15 +36,13 @@ class ApplicationController(
             ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorModel(400, "Company ID not found"))
 
         // TODO: HowTF do i take the JWT here and yeet that into this below
-        //val userObj = converter.convertUser()
+        val decoded = jwtDecoder.decode(token.substring(7))
 
-        // TODO: How do we get the list of contacts from the many to many relationship
-        // Actually... Thinking about this even more... I don't think the application cares
-        // about this knowledge? It's not even added on the submit application. Will remove for now
-        // just gonna pass a null list for now
-        //val contactObjs = converter.convertContactList(application.contacts)
+        // val userObj = converter.convertUser()
+        // userRepository.findByEmail(decoded)
 
-        val saved = applicationRepository.save(application.toApplicationEntity(companyObj, userObj))
-        return ResponseEntity.status(HttpStatus.CREATED).body(saved)
+        // val saved = applicationRepository.save(application.toApplicationEntity(companyObj, userObj))
+        // return ResponseEntity.status(HttpStatus.CREATED).body(saved)
+        return ResponseEntity.status(HttpStatus.CREATED).body("") // DEBUG
     }
 }
