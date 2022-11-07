3a3946ffbd61727c109f2720e77c9c354293eaa0
Dakota Junkman <dakota.junkman@gmail.com>
2022-11-07 15:02:16 -0600

    get skill

--- src/main/kotlin/com/jobtracker/api/controllers/UserSkillFrequencyController.kt
+++ src/main/kotlin/com/jobtracker/api/controllers/UserSkillFrequencyController.kt
@@ -10,11 +10,16 @@ import com.jobtracker.api.business.Helpers
 import com.jobtracker.api.controllers.models.ErrorModel
 import com.jobtracker.api.controllers.models.UserSkillFrequencyModel
 import com.jobtracker.api.repository.UserRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestParam
+import java.util.*
 
 @RestController
 @RequestMapping("/api")
@@ -42,5 +47,22 @@ class UserSkillFrequencyController(
 
         val saved = userSkillFrequencyRepository.save(skill.toUserSkillFrequencyEntity(user, 1))
         return ResponseEntity.status(HttpStatus.CREATED).body(saved)
+    }
+
+    @GetMapping("/skills/{skillId}")
+    fun getSkill(
+        @PathVariable skillId: String,
+        @RequestHeader("Authorization") token: String
+    ): ResponseEntity<Any> {
+        val user = Helpers.getUserByJWT(token, jwtDecoder, userRepository)
+            ?: return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorModel(404, "User not found"))
+
+        val skill = userSkillFrequencyRepository.findByIdOrNull(UUID.fromString(skillId))
+            ?: return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorModel(404, "Skill not found"))
+
+        if (skill.user.id != user.id)
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ErrorModel(403, "User cannot access this skill"))
+
+        return ResponseEntity.status(HttpStatus.OK).body(skill)
     }
 }
